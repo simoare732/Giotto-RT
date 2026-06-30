89cb3fb1ebdbafb361e324bdf7060ddf9ec19727
simoare732 <simone.aresta03@gmail.com>
2026-06-30 12:25:18 +0200

    Added periodicity in tasks

--- mc/mc.ino
+++ mc/mc.ino
@@ -33,7 +33,162 @@ enum ReadState{
   WAIT_END
 };
 
+const uint8_t T_te = 20;  // Task Engine Period
+const uint8_t T_ape = 20;  // Aperiodic Server Period
+
 // Function to calculate checksum using XOR operation
+byte calcChecksum(const byte* data, byte len);
+
+// Function to read a packet from the serial port
+bool readPacket(byte* data, byte& len);
+
+// Function to send a packet over the serial port
+void sendPacket(const char* msg);
+
+// Task to receive packets from the serial port and put them into the queue
+void TaskRec(void *pvParameters);
+
+// Task to process points from the queue and control the servo
+void TaskEngine(void *pvParameters);
+
+void setup() {
+  Serial.begin(9600);
+  pen.attach(pinPen);
+
+  queue = xQueueCreate(20, sizeof(Point));
+      
+  xTaskCreate(TaskRec, "TaskRx", 220, NULL, 2, NULL);
+  xTaskCreate(TaskEngine, "TaskEngine", 220, NULL, 3, NULL);
+  vTaskStartScheduler();
+}
+
+void loop() {}
+
+
+void TaskRec(void *pvParameters) {
+  static ReadState state = WAIT_START;
+  static byte expectedLen = 0;
+  static byte idx = 0;
+  static byte checksum = 0;
+
+
+  unsigned long tempoInizio;
+  unsigned long tempoFine;
+  unsigned long tempoEsecuzione;
+  
+  // Local buffer of task to store payload
+  byte data[MAX_PAYLOAD]; 
+
+  for (;;) {
+    if (Serial.available() > 0) {
+      byte b = Serial.read();
+
+      switch (state) {
+        case WAIT_START:
+          if (b == START_MARKER) state = READ_LEN;
+          break;
+
+        case READ_LEN:
+          expectedLen = b;
+          if (expectedLen == 0 || expectedLen > MAX_PAYLOAD) {
+            state = WAIT_START;
+          } else {
+            idx = 0;
+            checksum = expectedLen;
+            state = READ_PAYLOAD;
+          }
+          break;
+
+        case READ_PAYLOAD:
+          data[idx++] = b;
+          checksum ^= b;
+          if (idx >= expectedLen) state = READ_CHECKSUM;
+          break;
+
+        case READ_CHECKSUM:
+          if (b == checksum) state = WAIT_END;
+          else state = WAIT_START;
+          break;
+
+        case WAIT_END:
+          if (b == END_MARKER) {
+
+            tempoInizio = micros();
+            
+            data[idx] = '\0';
+            Point newP;
+
+            int tempDx = 0;
+            int tempSx = 0;
+            int tempState = 0;
+
+            // Read 3 points, and put them into the queue
+            int fields = sscanf((char*)data, "%d,%d,%d", &tempDx, &tempSx, &tempState);
+  
+            if (fields == 3) {
+              newP.angleDx = tempDx;
+              newP.angleSx = tempSx;
+              newP.state   = (tempState != 0);
+
+              xQueueSend(queue, &newP, portMAX_DELAY); 
+              
+            }
+
+            tempoFine = micros();
+            tempoEsecuzione = tempoFine - tempoInizio;
+
+            //char ackBuffer[32];
+            //sprintf(ackBuffer, "Time TaskRec: %lu", tempoEsecuzione);
+            //sendPacket(ackBuffer);
+            
+            state = WAIT_START;
+          } else {
+            // Reset state if END_MARKER does not correspond
+            state = WAIT_START;
+          }
+          break;
+      }
+    } 
+    else {
+      // If there are no data, task blocks
+      vTaskDelay(1);
+    }
+  }
+}
+
+
+void TaskEngine(void *pvParameters){
+  Point p;
+  char ackBuffer[32];
+
+  unsigned long tempoInizio;
+  unsigned long tempoFine;
+  unsigned long tempoEsecuzione;
+
+  // The time of last wake up
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  const TickType_t xPeriod = pdMS_TO_TICKS(T_te); // Period of task in ticks
+
+  for (;;) {
+
+    if (xQueueReceive(queue, &p, 0) == pdPASS) {
+      //tempoInizio = micros();
+      pen.write(p.angleDx);
+
+      //tempoFine = micros();
+      //tempoEsecuzione = tempoFine - tempoInizio;
+
+      
+      sprintf(ackBuffer, "Angle set to: %d", p.angleDx);
+      sendPacket(ackBuffer);
+    }
+
+    vTaskDelayUntil(&xLastWakeTime, xPeriod); // Wait for the next cycle
+  }
+}
+
+
+
 byte calcChecksum(const byte* data, byte len) {
   byte cs = 0;
   for (byte i = 0; i < len; i++) 
@@ -41,7 +196,7 @@ byte calcChecksum(const byte* data, byte len) {
   return cs;
 }
 
-// Function to read a packet from the serial port
+
 bool readPacket(byte* data, byte& len) {
   static ReadState state = WAIT_START;
   static byte expectedLen = 0;
@@ -92,7 +247,6 @@ bool readPacket(byte* data, byte& len) {
   return false;
 }
 
-// Function to send a packet over the serial port
 void sendPacket(const char* msg) {
   byte len = strlen(msg);
   if (len > MAX_PAYLOAD) 
@@ -111,110 +265,4 @@ void sendPacket(const char* msg) {
   Serial.write(cs);
   Serial.write(END_MARKER);
   Serial.flush();
-}
-
-void TaskRec(void *pvParameters) {
-  static ReadState state = WAIT_START;
-  static byte expectedLen = 0;
-  static byte idx = 0;
-  static byte checksum = 0;
-  
-  // Local buffer of task to store payload
-  byte data[MAX_PAYLOAD]; 
-
-  for (;;) {
-    if (Serial.available() > 0) {
-      byte b = Serial.read();
-
-      switch (state) {
-        case WAIT_START:
-          if (b == START_MARKER) state = READ_LEN;
-          break;
-
-        case READ_LEN:
-          expectedLen = b;
-          if (expectedLen == 0 || expectedLen > MAX_PAYLOAD) {
-            state = WAIT_START;
-          } else {
-            idx = 0;
-            checksum = expectedLen;
-            state = READ_PAYLOAD;
-          }
-          break;
-
-        case READ_PAYLOAD:
-          data[idx++] = b;
-          checksum ^= b;
-          if (idx >= expectedLen) state = READ_CHECKSUM;
-          break;
-
-        case READ_CHECKSUM:
-          if (b == checksum) state = WAIT_END;
-          else state = WAIT_START;
-          break;
-
-        case WAIT_END:
-          if (b == END_MARKER) {
-            
-            data[idx] = '\0';
-            Point newP;
-
-            int tempDx = 0;
-            int tempSx = 0;
-            int tempState = 0;
-
-            // Read 3 points, and put them into the queue
-            int fields = sscanf((char*)data, "%d,%d,%d", &tempDx, &tempSx, &tempState);
-  
-            if (fields == 3) {
-              newP.angleDx = tempDx;
-              newP.angleSx = tempSx;
-              newP.state   = (tempState != 0);
-
-              xQueueSend(queue, &newP, portMAX_DELAY); 
-              
-            }
-            
-            state = WAIT_START;
-          } else {
-            // Reset state if END_MARKER does not correspond
-            state = WAIT_START;
-          }
-          break;
-      }
-    } 
-    else {
-      // If there are no data, task blocks
-      vTaskDelay(pdMS_TO_TICKS(1));
-    }
-  }
-}
-
-
-void TaskEngine(void *pvParameters){
-  Point p;
-  ackBuffer[32];
-
-  for (;;) {
-    if (xQueueReceive(queue, &p, portMAX_DELAY) == pdPASS) {
-      pen.write(p.angleDx);
-      
-      sprintf(ackBuffer, "Angle set to %d", p.angleDx);
-      sendPacket(ackBuffer);
-    }
-  }
-}
-
-void setup() {
-  Serial.begin(9600);
-  pen.attach(pinPen);
-
-  queue = xQueueCreate(20, sizeof(Point));
-      
-  xTaskCreate(TaskRec, "TaskRx", 220, NULL, 2, NULL);
-  xTaskCreate(TaskEngine, "TaskEngine", 220, NULL, 3, NULL);
-  vTaskStartScheduler();
-}
-
-void loop() {
 }
